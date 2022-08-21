7cfdbd6937b07dc4a346600f53d61dbab44f968e
naszur27 <85011815+rizha2@users.noreply.github.com>
2022-08-21 15:27:52 +0800

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -14,3 +14,7 @@ echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"
 bash acme.sh --register-account -m kimochilol@gmail.com
 bash acme.sh --issue --standalone -d $domain --force
 bash acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key
+echo Done
+sleep 0.5
+clear
+neofetch
